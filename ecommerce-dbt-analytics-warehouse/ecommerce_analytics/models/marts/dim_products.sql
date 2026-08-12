ec006eebd035ba4f3c5b3d7a957acae101648542
Iliass Gzouli <iliassgzouli@gmail.com>
2026-08-12 17:26:07 +0100

    Enrich product dimension with category translations

--- ecommerce-dbt-analytics-warehouse/ecommerce_analytics/models/marts/dim_products.sql
+++ ecommerce-dbt-analytics-warehouse/ecommerce_analytics/models/marts/dim_products.sql
@@ -1,11 +1,24 @@
 {{ config(
     materialized='table'
-)}}
+) }}
 
 with products as (
+
     select *
-    from{{ ref('stg_products')}}
+    from {{ ref('stg_products') }}
+
+),
+
+category_translation as (
+
+    select *
+    from {{ ref('stg_product_category_translation') }}
+
 )
 
-select *
+select
+    products.*,
+    category_translation.product_category_name_english
 from products
+left join category_translation
+    on products.product_category_name = category_translation.product_category_name
---
+++ ecommerce-dbt-analytics-warehouse/ecommerce_analytics/models/staging/stg_product_category_translation.sql
@@ -0,0 +1,2 @@
+select *
+from read_parquet('../data/silver/product_category_name_translation.parquet')
