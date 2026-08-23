82d111584b7b667ea9e5758dff0b37f653e8a5a0
Iliass Gzouli <iliassgzouli@gmail.com>
2026-08-23 01:33:54 +0100

    Enrich order items with product category

--- ecommerce-dbt-analytics-warehouse/ecommerce_analytics/models/marts/fct_order_items.sql
+++ ecommerce-dbt-analytics-warehouse/ecommerce_analytics/models/marts/fct_order_items.sql
@@ -1,12 +1,26 @@
-{{config(
+{{ config(
     materialized='table'
-)}}
+) }}
 
 with order_items as (
 
     select *
-    from {{ ref('stg_order_items')}}
+    from {{ ref('stg_order_items') }}
+
+),
+
+products as (
+
+    select
+        product_id,
+        product_category_name_english
+    from {{ ref('dim_products') }}
+
 )
 
-select *
+select
+    order_items.*,
+    products.product_category_name_english
 from order_items
+left join products
+    on order_items.product_id = products.product_id
